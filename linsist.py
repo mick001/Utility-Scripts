2cc4eaf4da293faaf1618f8d9b7b72ed8dc1293f
Michy <mick001@users.noreply.github.com>
2016-08-21 23:38:12 +0200

    Update linsist.py

--- linsist.py
+++ linsist.py
@@ -2,28 +2,32 @@
 """
 Created on Fri Dec 18 15:52:18 2015
 
-@author: michy
+@author: Michy
 @description:
+    Simple function to solve linear systems
 
-# Simple function to solve linear systems
-#
-#   Y = A X
-#
-# by appling
-#
-#   X = A^(-1) Y
-#
-# where
-#       Y: column vector of known terms
-#       A: coefficient matrix (squared non singular matrix)
-#       X: unknown terms vector
+        Y = A X
+
+    by appling
+
+        X = A^(-1) Y
+
+    where
+        Y: column vector of known terms
+        A: coefficient matrix (squared non singular matrix)
+        X: unknown terms vector
+
+@example:
+
+    A = np.matrix('1 2; 3 4')
+    y = np.matrix('2; 4')
+    print(solveSystem(y,A))
 
 """
 
 import numpy as np
 
-# Solve function
-
+# Solver function
 def solveSystem(y,A):
 
     if np.linalg.det(A) == 0:
@@ -35,8 +39,3 @@ def solveSystem(y,A):
         else:
             raise ValueError("Number of columns of A must be equal to the number of rows of y!")
 
-
-# Example
-A = np.matrix('1 2; 3 4')
-y = np.matrix('2; 4')
-print(solveSystem(y,A))
